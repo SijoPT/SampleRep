0b80501de6c9433cdc8557604a44cb64de268f07
SijoThomas <sijo.pt@gadgeon.com>
2016-10-13 14:52:55 +0530

    HelloWorld App Created

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Sijo Thomas on 13/10/16.
+//  Copyright © 2016 Sijo Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    print("HelloWorld")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

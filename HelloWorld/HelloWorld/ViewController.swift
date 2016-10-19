c0306bef63220201ef52980bc450cc8d0def6bcd
SijoThomas <sijo.pt@gadgeon.com>
2016-10-19 12:42:51 +0530

    updte

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -30,28 +30,6 @@ class ViewController: UIViewController {
         print("HelloWorld")
         print("HelloWorld")
         
-        print("HelloWorld")
-        print("HelloWorld")
-        print("HelloWorld")
-        print("HelloWorld")
-        print("HelloWorld")
-        print("HelloWorld")
-        print("HelloWorld")
-        print("HelloWorld")
-        print("HelloWorld")
-        print("HelloWorld")
-        
-        print("HelloWorld")
-        print("HelloWorld")
-        print("HelloWorld")
-        print("HelloWorld")
-        print("HelloWorld")
-        print("HelloWorld")
-        print("HelloWorld")
-        print("HelloWorld")
-        print("HelloWorld")
-        print("HelloWorld")
-        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
